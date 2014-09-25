312598a296ead9b0be3a4f8a180b97e0f47771b9
leafileaf <leafileaftfm@gmail.com>
2014-09-25 21:41:52 +0800

    :)
    testing ipp preprocessing

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 		<script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		<?php
-			include "_navbar.html";
-		?>
+		!ipp[_cep14_insert _navbar.html]
 	</body>
 </html>
