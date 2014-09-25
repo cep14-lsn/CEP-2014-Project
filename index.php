b173cbb78af3549141ac0963188299cc0a3cf1d3
leafileaf <leafileaftfm@gmail.com>
2014-09-25 16:49:06 +0800

    :)

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
+		<script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<?php
+			include "_navbar.html";
+		?>
+	</body>
+</html>
