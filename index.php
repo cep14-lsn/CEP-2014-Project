411c08bfc2a39c5b8a32d997ee4fb1b70f9d586d
leafileaf <leafileaftfm@gmail.com>
2014-09-25 21:50:40 +0800

    :)
    made the head tags thingy also set pagename to home

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
+!ipp[setv pagename Home]
 <html>
 	<head>
-		<link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
-		<script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		!ipp[_cep14_insert _head.html]
 	</head>
 	<body>
 		!ipp[_cep14_insert _navbar.html]
