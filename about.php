30ec8d5e3bb5a7e6b82d774fb0b47ad786ec3dd1
leafileaf <leafileaftfm@gmail.com>
2014-09-30 20:40:59 +0800

    :)
    restored previous version

--- about.php
+++ about.php
@@ -5,9 +5,6 @@
 	</head>
 	<body>
 		!ipp[_cep14_insert _navbar.html]
-		<div class = "container">
-			Of course, we have some stuff here, yeah?
-		</div>
 		!ipp[_cep14_insert _footer.html]
 	</body>
 </html>
