42ac326bc4dc465dab4837377404187f36741842
Lhwhatever <llinhong.2301@gmail.com>
2014-10-02 20:49:03 +0800

    Fixed non-inclusion of CSS
    Fixed incorrect usage of IPP to link CSS

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		!ipp[_cep14_insert components/_head.html]
-		!ipp[_cep14_insert css/_styles.css]
+		<link rel="stylesheet" href="css/_carousel.css" type="text/css"/>
 	</head>
 	<body>
 		!ipp[_cep14_insert components/_navbar.html]
