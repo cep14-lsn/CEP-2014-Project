a19b1e6f0b2de378bdf019b1b2197db19dd814e5
Lhwhatever <llinhong.2301@gmail.com>
2014-10-02 22:13:58 +0800

    Script experimentation
    Created active scripts JS file for future scripts that will run on
    document ready. Added replace span text function as placeholder
    function.

--- about.php
+++ about.php
@@ -8,7 +8,7 @@
     <div>
         <h1>So, who are we?</h1>
         <p>We are a group of pioneers who want to get rid of the notion of oily fast food.</p>
-        <p>We believe in low-fat food that can be both enjoyable and tasty. With our new <span class="redacted">[REDACTED]</span> technology, enjoy burgers and fries with unbelievably low fat values!<p>
+        <p>We believe in low-fat food that can be both enjoyable and tasty. With our new <span class="span-topSecret"></span> technology, enjoy burgers and fries with unbelievably low fat values!<p>
         <p><a class="btn btn-primary" href="menu.php" role="button">Low Fat Food Menu <span class="glyphicon glyphicon-chevron-right"></span></a></p>
     </div>
     !ipp[_cep14_insert components/_footer.html]
