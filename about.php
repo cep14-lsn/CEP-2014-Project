14a030f369339f8758c49acb1f7f2314366017f6
Lhwhatever <llinhong.2301@gmail.com>
2014-10-02 21:55:17 +0800

    Fixing nonstandard font sizes
    1. Jumbotron classes will be eradiacted shortly.
    2. Font color on hovering above buttons should be fixed.

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     !ipp[_cep14_insert components/_navbar.html]
-    <div class="jumbotron" id="preamble">
+    <div>
         <h1>So, who are we?</h1>
         <p>We are a group of pioneers who want to get rid of the notion of oily fast food.</p>
         <p>We believe in low-fat food that can be both enjoyable and tasty. With our new <span class="redacted">[REDACTED]</span> technology, enjoy burgers and fries with unbelievably low fat values!<p>
