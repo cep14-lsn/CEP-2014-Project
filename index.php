5b6c33b72d28c4f0ee45eb947428d376a60f6f00
leafileaf <leafileaftfm@gmail.com>
2014-10-02 15:46:48 +0800

    :)
    for the sake of readability

--- index.php
+++ index.php
@@ -7,40 +7,39 @@
 		!ipp[_cep14_insert _navbar.html]
 		!ipp[_cep14_insert _footer.html]
 		<div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
 			<ol class="carousel-indicators">
-    			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		        <li data-target="#myCarousel" data-slide-to="1"></li>
-		        <li data-target="#myCarousel" data-slide-to="2"></li>
-	      	</ol>
-	      	<div class="carousel-inner">
-	        	<div class="item active">
-		          <img src="mclarenp1_c.jpg" alt="First slide">
-		          <div class="container">
-		            <div class="carousel-caption">
-		              <!--<p><a class="btn btn-lg btn-primary" href="#" role="button"></a></p>-->
-		            </div>
-		          </div>
-		        </div>
-		        <div class="item">
-		          <img src="mclarenp1_b.jpg" alt="Second slide">
-		          <div class="container">
-		            <div class="carousel-caption">
-		              <!--<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>-->
-		            </div>
-		          </div>
-		        </div>
-		        <div class="item">
-		          <img src="mclarenp1.jpg" alt="Third slide">
-		          <div class="container">
-		            <div class="carousel-caption">
-		              <!--<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>-->
-		            </div>
-		          </div>
-		        </div>
-		      </div>
-		      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
-		      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
-	  </div><!-- /.carousel -->
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="mclarenp1_c.jpg" alt="First slide" />
+					<div class="container">
+						<div class="carousel-caption">
+							<!--<p><a class="btn btn-lg btn-primary" href="#" role="button"></a></p>-->
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<img src="mclarenp1_b.jpg" alt="Second slide" />
+					<div class="container">
+						<div class="carousel-caption">
+							<!--<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>-->
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<img src="mclarenp1.jpg" alt="Third slide" />
+					<div class="container">
+						<div class="carousel-caption">
+							<!--<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>-->
+						</div>
+					</div>
+				</div>
+			</div>
+			<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+			<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+		</div>
 	</body>
 </html>
