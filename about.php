edb9af5887fcbee650b1a3deeef94048708ef143
neonrsa <nelson.infinity@gmail.com>
2014-10-03 10:04:04 +0800

    Update about.php

--- about.php
+++ about.php
@@ -8,14 +8,14 @@
     <div class = "container">
         <div class="container-fluid">
             <h1>So, who are we?</h1>
-            <p>We are a group of pioneers who want to get rid of the notion of oily fast food.</p>
+            <p>We are a group of pioneers who want to get rid of the notion of oily fast food. :)</p>
             <p>We believe in low-fat food that can be both enjoyable and tasty. With our new <span class="span-topSecret">[REDACTED]</span> technology, enjoy burgers and fries with unbelievably low fat values!<p>
             <p><a class="btn btn-primary" href="menu.php" role="button">Low Fat Food Menu <span class="glyphicon glyphicon-chevron-right"></span></a></p>
         </div>
         <hr>
         <div class="container-fluid">
             <h1>Our Story</h1>
-            <p>MekDoornels started in the year 1939, founded by the LSN Foundation as an experimental restaurant chain.</p>
+            <p>MekDoornels started in the year 1923, founded by the LSN Foundation as an experimental restaurant chain. Our first test subjects, er, customers were willing and daring volunteers who signed up for our tasty<span style="font-size: 2px">-ass</span> programme.</p>
             <p>The restaurant chain was intended to produce food for customers as fast as possible.</p>
             <p>In the first 6 years of our operation, just as the world struggled to survive through one of the most deadly wars ever, we struggled to stay competitive in the ever-changing restaurant industry.</p>
             <p>However, ever since Pear Inc. sponsored our growth in the year 1965, our business has continued to skyrocket, until you see the sucess that is us today.</p>
@@ -27,7 +27,7 @@
             <p>We intend to pioneer the food industry to bring food to new levels of enjoyability and nutrition.</p>
             <p>Our breakthrough invention, the <span class="span-topSecret">[REDACTED]</span> technology allows us to reduce fat values in foot to near zero levels, elevating our foodstuffs to a even greater level in health.</p>
             <p><a class="btn btn-primary" href="menu.php" role="button">Try a Sample <span class="glyphicon glyphicon-chevron-right"></span></a></p>
-            <p>We are also looking for people to take part in our culinary experiments! All our released-for-testing foodstuffs have been extensively tested and proven safe by over 100 organizations. Join us as we need your advice and opinion to help make our food the best for everyone!</p>
+            <p>We are also looking for people to take part in our culinary experiments! All our released-for-testing foodstuffs have been extensively tested and proven safe by over 100 organizations, including the Food Action for Trade and Security a.k.a. FATS. Join us to contribute your valuable advice and opinion to help make our food the best for everyone!</p>
             <p><a class="btn btn-primary" href="#" role="button">Sign up as Culinary Tester <span class="glyphicon glyphicon-cheveron-right"></span></a></p>
         </div>
     </div>
