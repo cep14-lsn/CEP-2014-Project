f55c646d8b63067bb0628df76d9ed6098c9908b6
leafileaf <leafileaftfm@gmail.com>
2014-10-03 21:10:25 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -115,7 +115,7 @@
 				</div>
 				<div class = "col-xs-12 col-sm-9 contact-info">
 					<strong>{{ p.name }}</strong>
-					<a class = "text-muted" ng-href = "mailto:{{ p.email }}"><i>{{ p.email }}</i></a>
+					<p class = "text-muted"><i><a ng-href = "mailto:{{ p.email }}">{{ p.email }}</a></i></p>
 					<p>H {{ p.contact.home }} / M {{ p.contact.mobile}}</p>
 				</div>
 			</div>
