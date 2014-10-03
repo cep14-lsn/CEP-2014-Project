39ae58c2579c588677d789c8cdeffb77050c6f05
neonrsa <nelson.infinity@gmail.com>
2014-10-03 15:59:30 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -7,11 +7,9 @@
 		!ipp[_cep14_insert components/_navbar.html]
 		!ipp[_cep14_insert components/_footer.html]
         	<h1>Tasty food selections below!</h1>
-	        <div class="container-fluid">
-			<div class = "foodview">
-				<img src = "Tripleburger.png">
-				<p class = "fooddescription"><strong>TRIPLEBURGER</strong><br>Calories: 10<br>Taste: 10/10<br>Smell: Overwhelmingly Pungent</p>
-			</div>
-	        </div>
+		<div class = "foodview">
+			<img src = "Tripleburger.png">
+			<p class = "fooddescription"><strong>TRIPLEBURGER</strong><br>Calories: 10<br>Taste: 10/10<br>Smell: Overwhelmingly Pungent</p>
+		</div>
 	</body>
 </html>
