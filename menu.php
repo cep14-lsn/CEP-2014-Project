aa93e8fce4c6dce661cd0e41e521e54e71f177f2
leafileaf <leafileaftfm@gmail.com>
2014-10-03 16:53:15 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -5,8 +5,7 @@
 	</head>
 	<body>
 		!ipp[_cep14_insert components/_navbar.html]
-		<h1>Menu</h1>
-		<div class = "page-header">
+		<div class = "page-header" id = "burgers">
 			<h1>Burgers <small>a collection of burgers to satisfy your gastronomic needs</small></h1>
 		</div>
 		<div class = "row">
@@ -20,12 +19,26 @@
 				</p>
 			</div>
 		</div>
-		<div class = "page-header">
-			<h1>Finger food <small>food to eat on-the-go, or as a snack</small></h1>
+		<div class = "page-header" id = "sidedishes">
+			<h1>Side Dishes <small>food to eat on-the-go, or as a snack</small></h1>
 		</div>
 		<div class = "row">
 			<div class = "col-xs-6 col-md-3 food-item">
 				<img src = "megafries.png" />
+				<p class = "food-desc">
+					<strong>MEGA FRIES</strong><br>
+					Calories : 5<br>
+					Taste : 10 / 10<br>
+					Smell : Mouthwateringly Savoury
+				</p>
+			</div>
+		</div>
+		<div class = "page-header" id = "setmeals">
+			<h1>Set Meals <small>meals to have anywhere, complete with drink and side dish</small></h1>
+		</div>
+		<div class = "row">
+			<div class = "col-xs-6 col-md-3 food-item">
+				<img src = "burgercombo.gif" />
 				<p class = "food-desc">
 					<strong>MEGA FRIES</strong><br>
 					Calories : 5<br>
