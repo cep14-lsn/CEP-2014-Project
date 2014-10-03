cd341b40f12f6a7a11c509e985eeacdd7af082e1
Lhwhatever <llinhong.2301@gmail.com>
2014-10-03 16:17:59 +0800

    More stuff in about.php

--- about.php
+++ about.php
@@ -18,7 +18,9 @@
             <p>MekDoornels started in the year 1939, founded by the LSN Foundation as an experimental restaurant chain. Our first test subjects, er, customers were willing and daring volunteers who signed up for our tasty programme.</p>
             <p>The restaurant chain was intended to produce food for customers as fast as possible.</p>
             <p>In the first 6 years of our operation, just as the world struggled to survive through one of the most deadly wars ever, we struggled to stay competitive in the ever-changing restaurant industry.</p>
-            <p>However, ever since Pear Inc. sponsored our growth in the year 1965, our business has continued to skyrocket, until you see the <span style="font-size: 0.1pt">increadibly dubious and crappy</span>success that is us today.</p>
+            <p>However, ever since Pear Inc. sponsored our growth in the year 1965, our business has continued to skyrocket, until you see the success that is us today.</p>
+            <p>By the year 1984, we have expanded to 3 continents <!--Oceania, Eurasia, Eastasia (1984 reference)--> and our yearly revenue had increased to USD 1 quadrillion!</p>
+            <p>Till today, our business still flourishes everywhere we are. We look forward to building a brighter future with you.</p>
         </div>
         <hr>
         <div class="container-fluid">
@@ -29,6 +31,20 @@
             <p><a class="btn btn-primary" href="menu.php" role="button">Try a Sample <span class="glyphicon glyphicon-chevron-right"></span></a></p>
             <p>We are also looking for people to take part in our culinary experiments! All our released-for-testing foodstuffs have been extensively tested and proven safe by over 100 organizations, including the Food Action for Trade and Security a.k.a. FATS. Join us to contribute your valuable advice and opinion to help make our food the best for everyone!</p>
             <p><a class="btn btn-primary" href="contact.php" role="button">Sign up as Culinary Test Subject <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+        </div>
+        <hr>
+        <div class="container-fluid">
+            <h1>Working with Us</h1>
+            <p>MekDoornels is an equal-opportunities employer. We have a variety of vacancies for many specialties as part of our efforts to expand further and experiment further. If you or a friend need a job, do not refrain from contacting us! As part of our global outreach programme, employees and their families have many special privileges and perks that come with being a part of the MekDoornels family. Some of them include a 30% discount at all MekDoornels restaurants!</p>
+            <p><a class="btn btn-primary" href="contact.php" role="button">Contact us for opportunities <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+            <h2>Culinary Experimenter</h2>
+            <p>At the forefront of this field, we will need a lot of people to help us push the boundaries of food. You can sign us as either one of the following:</p>
+            <ul>
+                <li><span class="bold">Follower</span> to give us ideas that we can consider exploring. Mr X, a customer for 30 years, just suggested asparagus ice cream in the recent few months, and it is now a success on the market!</p>
+                <li><span class="bold">Food connoisseur</span> to help suggest new ways to refine our current food.</li>
+                <li><span class="bold">Food researcher</span> to help spot trends and start trends for new forms of food!</li>
+                <li><span class="bold">Food tester</span> to help judge our food and appraise it for the market! You can either help certify the safety of the food, or appraise its taste!</li>
+            </ul>
         </div>
     </div>
     !ipp[_cep14_insert components/_footer.html]
