c06085b4ee58c6bb444c085deca6570085877925
leafileaf <leafileaftfm@gmail.com>
2014-10-03 21:43:26 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -28,52 +28,62 @@
 		}
 		</style>
 	</head>
+	<script>
+		infoFood = jQuery.getJSON(($.getJSON("js/food_info.json", function(data) {return data})));
+		function menuCont( $scope ) {
+			$scope.foods = infoFood;
+			$scope.process = function ( l ) {
+				return l.join("<br>");
+			}
+		}
+	</script>
 	<body>
 		<!--<div class = "menu-desc">
 			<h1 style = "font-size:20pt"> A wide and delectable selection of fine foods!</h1>
 			<h2 style = "font-size:8pt"> P.S. If any food item starts a conversation with you, please incinerate it.</h2>-->
 		!ipp[_cep14_insert components/_navbar.html]
-
-
-		<div data-ng-app="list-menu" class="container-fluid">
-		    <h1>Set meals</h1>
-		    <p>These are meals to have anywhere. You can either purchase it &agrave; la carte or as a full meal.</p>
-		    <div data-ng-controller="listMealSet">
-		        <div data-ng-repeat="mealSet in infoFood">
-		            <h2>{{mealSet.name}}</h2>
-		            <img data-ng-src="{{mealSet.img}}">
-		            <p>{{mealSet.finalDesc}}</p>
-		            <table>
-		                <tr>
-		                    <th>Substance</th>
-		                    <th>Amount</th>
-		                </tr>
-		                <tr>
-		                    <td>Energy</td>
-		                    <td>{{mealSet.nutrition.calorie}} calories</td>
-		                </tr>
-		                <tr>
-		                    <td>Carbohydrates</td>
-		                    <td>{{mealSet.nutrition.carbohydrate / 5}} g</td>
-		                </tr>
-		                <tr>
-		                    <td>Fats</td>
-		                    <td>{{mealSet.nutrition.fats}} mg</td>
-		                </tr>
-		                <tr>
-		                    <td>Proteins</td>
-		                    <td>{{mealSet.nutrition.protein / 5}} g</td>
-		                </tr>
-		                <tr>
-		                    <td>Sodium</td>
-		                    <td>{{mealSet.nutrition.salt}} mg</td>
-		                </tr>
-		            </table>
-
-		        </div>
-		    </div>
+		<div ng-app = "" ng-controller = "menuCont" class="container-fluid">
+			<div class = "container">
+				<div class = "page-header">
+					<h1>Set meals <small>to be had anywhere; à la carte or as a set meal</small></h1>
+				</div>
+				<div ng-repeat = "set in foods.mealSet">
+					<div class = "row">
+						<div class = "col-xs-12 col-md-6 meal-pic">
+							<img ng-src = "{{set.img}}" alt = "Meal image" />
+						</div>
+						<div class = "col-xs-12 col-md-6 meal-info">
+							<strong>{{ set.name }}</strong>
+							<p class = "text-muted"><i>{{ process( set.desc ) }}</i></p>
+							<table>
+								<tr>
+									<th>Nutritional Information</th>
+								</tr>
+								<tr>
+									<td>Energy</td>
+									<td>{{ set.nutrition.calorie }} kcal</td>
+								</tr>
+								<tr>
+									<td></td>
+									<td>{{ set.nutrition.calorie * 4.2 }} kJ</td>
+								<tr>
+									<td>Carbohydrates</td>
+									<td>{{ set.nutrition.carbohydrate / 5 }} g</td>
+								</tr>
+								<tr>
+									<td>Fats</td>
+									<td>{{ set.nutrition.fats }} mg</td>
+								</tr>
+								<tr>
+									<td>Protein</td>
+									<td>{{ set.nutrition.protein / 5 }} g</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-		<script src="js/menu.js"></script>
 		!ipp[_cep14_insert components/_footer.html]
 	</body>
 </html>
