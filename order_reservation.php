00357799ac4873e0fe5fdf5b73575fd488c90d49
Lhwhatever <llinhong.2301@gmail.com>
2014-10-03 23:58:11 +0800

    Fixed formatting of form tags

--- order_reservation.php
+++ order_reservation.php
@@ -7,6 +7,7 @@
 	<body>
 		!ipp[_cep14_insert components/_navbar.html]
 		<div class="container-fluid" ng-app="svcReserve" ng-controller="reserver">
+			<h1>Why reserve?</h1>
 			<p>Sometimes, it can be difficult for you and your friends or family to find a space just for you at the MekDoornels restaurant.</p>
 			<p>Popularity can be both a blessing and a curse.</p>
 			<p>However, for you, we have just the right solution - Seat Reservation!</p>
@@ -24,13 +25,13 @@
 				<option>Mt. Vesuvius Branch</option>
 				<option>The Rude Sandstorm</option>
 			</select></p>
-			<p>Total Tables: {{tablesTotal}}</p>
-			<p>Vacancies: <span id="vacancies">{{vacancies}}</span></p>
+			<p>Total Tables: {{ tablesTotal }}</p>
+			<p>Vacancies: <span id="vacancies">{{ vacancies }}</span></p>
 			<p>Reserve Tables: <input type="number" ng-model="intended"></p>
-			<p>Cost: {{cost|currency}}</p>
+			<p>Cost: {{ cost|currency }}</p>
 			<p><a class="btn btn-primary" href="#" onclick="deduct()">Process Deduction</a></p>
 			<br>
-			<p>Use this reservation code: {{reservation_code}}</p>
+			<p>Use this reservation code: {{ reservation_code }}</p>
 		</div>
 		<script>
 			var GST = 0.07;
