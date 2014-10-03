63ab4e1ba4fd530de067a93cfdea12e5a7f8b94c
leafileaf <leafileaftfm@gmail.com>
2014-10-03 16:50:12 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -6,14 +6,31 @@
 	<body>
 		!ipp[_cep14_insert components/_navbar.html]
 		<h1>Menu</h1>
+		<div class = "page-header">
+			<h1>Burgers <small>a collection of burgers to satisfy your gastronomic needs</small></h1>
+		</div>
 		<div class = "row">
 			<div class = "col-xs-6 col-md-3 food-item">
 				<img src = "Tripleburger.png" />
 				<p class = "food-desc">
 					<strong>TRIPLEBURGER</strong><br>
-					Calories: 10<br>
-					Taste: 10 / 10<br>
-					Smell : Overwhelmingly Pungent</p>
+					Calories : 10<br>
+					Taste : 10 / 10<br>
+					Smell : Overwhelmingly Pungent
+				</p>
+			</div>
+		</div>
+		<div class = "page-header">
+			<h1>Finger food <small>food to eat on-the-go, or as a snack</small></h1>
+		</div>
+		<div class = "row">
+			<div class = "col-xs-6 col-md-3 food-item">
+				<img src = "megafries.png" />
+				<p class = "food-desc">
+					<strong>MEGA FRIES</strong><br>
+					Calories : 5<br>
+					Taste : 10 / 10<br>
+					Smell : Mouthwateringly Savoury
 				</p>
 			</div>
 		</div>
