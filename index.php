da8c01e53aa9b62e83396ab19edb86b473979756
Lhwhatever <llinhong.2301@gmail.com>
2014-10-03 16:32:35 +0800

    Fixed some stuff
    1. <hr> in index.php
    2. Made bolded text more obvious.

--- index.php
+++ index.php
@@ -40,55 +40,53 @@
 	<body>
 		!ipp[_cep14_insert components/_navbar.html]
 		<div class = "container">
-		    <h1>Welcome!</h1>
-		    <p>We are the leading franchise in the F&B industry and we are honored to have you on our site.</p>
-		    <p>We bring out the best in fast food. For example, look at our brand new Triple Burger with fat amount totalling to 0.1 mg!</p>
-		    <p><a class="btn btn-primary" href="about.php">More About Us <span class="glyphicon glyphicon-chevron-right"></span></a> <a class="btn btn-primary" href="menu.php">Our Wonderful Food <span class="glyphicon glyphicon-chevron-right"></span></a></p>
-		</div>
+        <h1>Welcome!</h1>
+        <p>We are the leading franchise in the F&B industry and we are honored to have you on our site.</p>
+        <p>We bring out the best in fast food. For example, look at our brand new Triple Burger with fat amount totalling to 0.1 mg!</p>
+        <p><a class="btn btn-primary" href="about.php">More About Us <span class="glyphicon glyphicon-chevron-right"></span></a> <a class="btn btn-primary" href="menu.php">Our Wonderful Food <span class="glyphicon glyphicon-chevron-right"></span></a></p>
 		<hr>
-		<div class = "container">
-		    <h1>Why Choose Us?</h1>
-			<div id = "foodcarousel" class = "carousel slide" data-ride = "carousel">
-				<ol class = "carousel-indicators">
-					<li data-target = "#foodcarousel" data-slide-to = "0" class = "active"></li>
-					<li data-target = "#foodcarousel" data-slide-to = "1"></li>
-					<li data-target = "#foodcarousel" data-slide-to = "2"></li>
-				</ol>
-				<div class = "carousel-inner">
-					<div class = "item active">
-						<img src = "burgercombo.gif" alt = "First slide" />
-						<div class = "container">
-							<div class = "carousel-caption">
-								<h1>Set Meals</h1>
-								<p>Explore our diverse set meal options that will fill you up for low cost.</p>
-								<p><a class = "btn btn-lg btn-primary" href = "menu.php" role = "button">Explore the menu <span class="glyphicon glyphicon-chevron-right"></span></a></p>
-							</div>
-						</div>
-					</div>
-					<div class = "item">
-						<img src = "megafries.png" alt = "Second slide" />
-						<div class = "container">
-							<div class = "carousel-caption">
-								<h1>Fries</h1>
-								<p>Our fries are known all around the region. Grab some fries and share them with friends, or eat them yourself!</p>
-								<p><a class = "btn btn-lg btn-primary" href = "menu.php" role = "button">Fries and other food <span class="glyphicon glyphicon-chevron-right"></span></a></p>
-							</div>
-						</div>
-					</div>
-					<div class = "item">
-						<img src = "Tripleburger.png" alt = "Third slide" />
-						<div class = "container">
-							<div class = "carousel-caption">
-								<h1>Burgers</h1>
-								<p>Huge burgers. Few calories. That's right, more great-tasting food and less calories! What could be better?</p>
-								<p><a class = "btn btn-lg btn-primary" href = "menu.php" role = "button">Find other healthy meal choices <span class="glyphicon glyphicon-chevron-right"></span></a></p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<a class = "left carousel-control" href = "#foodcarousel" role = "button" data-slide = "prev"><span class = "glyphicon glyphicon-chevron-left"></span></a>
-				<a class = "right carousel-control" href = "#foodcarousel" role = "button" data-slide = "next"><span class = "glyphicon glyphicon-chevron-right"></span></a>
-			</div>
+        <h1>Why Choose Us?</h1>
+        <div id = "foodcarousel" class = "carousel slide" data-ride = "carousel">
+            <ol class = "carousel-indicators">
+                <li data-target = "#foodcarousel" data-slide-to = "0" class = "active"></li>
+                <li data-target = "#foodcarousel" data-slide-to = "1"></li>
+                <li data-target = "#foodcarousel" data-slide-to = "2"></li>
+            </ol>
+            <div class = "carousel-inner">
+                <div class = "item active">
+                    <img src = "burgercombo.gif" alt = "First slide" />
+                    <div class = "container">
+                        <div class = "carousel-caption">
+                            <h1>Set Meals</h1>
+                            <p>Explore our diverse set meal options that will fill you up for low cost.</p>
+                            <p><a class = "btn btn-lg btn-primary" href = "menu.php" role = "button">Explore the menu <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class = "item">
+                    <img src = "megafries.png" alt = "Second slide" />
+                    <div class = "container">
+                        <div class = "carousel-caption">
+                            <h1>Fries</h1>
+                            <p>Our fries are known all around the region. Grab some fries and share them with friends, or eat them yourself!</p>
+                            <p><a class = "btn btn-lg btn-primary" href = "menu.php" role = "button">Fries and other food <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class = "item">
+                    <img src = "Tripleburger.png" alt = "Third slide" />
+                    <div class = "container">
+                        <div class = "carousel-caption">
+                            <h1>Burgers</h1>
+                            <p>Huge burgers. Few calories. That's right, more great-tasting food and less calories! What could be better?</p>
+                            <p><a class = "btn btn-lg btn-primary" href = "menu.php" role = "button">Find other healthy meal choices <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class = "left carousel-control" href = "#foodcarousel" role = "button" data-slide = "prev"><span class = "glyphicon glyphicon-chevron-left"></span></a>
+            <a class = "right carousel-control" href = "#foodcarousel" role = "button" data-slide = "next"><span class = "glyphicon glyphicon-chevron-right"></span></a>
+        </div>
 		</div>
 	  	<div class="container marketing">
 		<div class="row">
