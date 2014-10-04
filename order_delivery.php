aea4eedb592ec02040109ed70dc5208e78463745
leafileaf <leafileaftfm@gmail.com>
2014-10-04 22:26:30 +0800

    Update order_delivery.php

--- order_delivery.php
+++ order_delivery.php
@@ -20,7 +20,6 @@
 			var xhr = new XMLHttpRequest();
 			xhr.onreadystatechange = function() {
 				if(xhr.readyState == 4 && xhr.status == 200){
-					console.log(xhr.responseText);
 					infoFood = JSON.parse(xhr.responseText);
 				}
 			};
