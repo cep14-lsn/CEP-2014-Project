e4b27e5f5e036a48cd4e1dffac41d883b5ae59eb
leafileaf <leafileaftfm@gmail.com>
2014-10-05 16:45:03 +0800

    Update order_reservation.php

--- order_reservation.php
+++ order_reservation.php
@@ -145,8 +145,8 @@
 					<td>{{ locInfo[loc].cost * ir | currency }}</td>
 				</tr>
 			</table>
-			<a class="btn btn-primary" href="#" onclick = "return false;" data-ng-click="reserve()" data-ng-show = "canReserve()"><span class = "glyphicon glyphicon-ok-circle"></span> Process Deduction</a>
-			<a class="btn btn-primary" data-ng-hide = "canReserve" disabled = "disabled"><span class = "glyphicon glyphicon-ok-circle"></span> Process Deduction</a>
+			<a class="btn btn-primary" href="#" onclick = "return false;" data-ng-click="reserve()" data-ng-show = "canreserve"><span class = "glyphicon glyphicon-ok-circle"></span> Process Deduction</a>
+			<a class="btn btn-primary" data-ng-hide = "canreserve()" disabled = "disabled"><span class = "glyphicon glyphicon-ok-circle"></span> Process Deduction</a>
 			<div class = "panel panel-primary" data-ng-show = "reservations.length > 0">
 				<div class = "panel-heading">
 					Reservation
