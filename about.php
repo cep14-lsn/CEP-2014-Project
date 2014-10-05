8c206033741615135ad18e4e2112e3158900fec5
leafileaf <leafileaftfm@gmail.com>
2014-10-05 13:54:11 +0800

    Update about.php

--- about.php
+++ about.php
@@ -1,104 +1,104 @@
 !ipp[setv pagename About]
 <html>
 <head>
-    !ipp[_cep14_insert components/_head.html]
+	!ipp[_cep14_insert components/_head.html]
 </head>
 <body>
-    !ipp[_cep14_insert components/_navbar.html]
-    <div class = "container">
-        <div class = "page-header">
-                <h1>About <span class="text-small">so you can get to know more about us and have more confidence in eating our food</span></h1>
-            </div>
-        <div class="container-fluid">
-            <h1>So, who are we?</h1>
-            <p>We are a group of pioneers who want to get rid of the notion of oily fast food.</p>
-            <p>We believe in low-fat food that can be both enjoyable and tasty. With our new <span class="span-topSecret">[REDACTED]</span> technology, enjoy burgers and fries with unbelievably low fat values!<p>
-            <p><a class="btn btn-primary" href="menu.php" role="button">Low Fat Food Menu <span class="glyphicon glyphicon-chevron-right"></span></a></p>
-        </div>
-        <hr>
-        <div class="container-fluid">
-            <h1>Our Story</h1>
-            <p>MekDoornels started in the year 1939, founded by the LSN Foundation as an experimental restaurant chain. Our first test subjects, er, customers were willing and daring volunteers who signed up for our tasty programme.</p>
-            <p>The restaurant chain was intended to produce food for customers as fast as possible.</p>
-            <p>In the first 6 years of our operation, just as the world struggled to survive through one of the most deadly wars ever, we struggled to stay competitive in the ever-changing restaurant industry.</p>
-            <p>However, ever since Pear Inc. sponsored our growth in the year 1965, our business has continued to skyrocket, until you see the success that is us today.</p>
-            <p>By the year 1984, we have expanded to 3 continents <!--Oceania, Eurasia, Eastasia (1984 reference)--> and our yearly revenue had increased to USD 1 000 000 000 000!</p>
-            <p>Till today, our business still flourishes everywhere we are. We look forward to building a brighter future with you.</p>
-        </div>
-        <hr>
-        <div class="container-fluid">
-            <h1>Our Vision</h1>
-            <p>As we started up as an experimental restaurant chain, we still partake in culinary experiments to give our customers the very best.</p>
-            <p>We intend to pioneer the food industry to bring food to new levels of <span style="font-size: 1pt">questionable </span>enjoyability and <span style="font-size:1pt">zero </span>nutrition.</p>
-            <p>Our breakthrough invention, the <span class="span-topSecret">[REDACTED]</span> technology allows us to reduce fat values in foot to near zero levels, elevating our foodstuffs to a even greater level in health.</p>
-            <p><a class="btn btn-primary" href="menu.php" role="button">Try a Sample <span class="glyphicon glyphicon-chevron-right"></span></a></p>
-            <p>We are also looking for people to take part in our culinary experiments! All our released-for-testing foodstuffs have been extensively tested and proven safe by over 100 organizations, including the <abbr title="FATS">Food Association for Trade and Security</abbr>. Join us to contribute your valuable advice and opinion to help make our food the best for everyone!</p>
-            <p><a class="btn btn-primary" href="contact.php" role="button">Sign up as Culinary Test Subject <span class="glyphicon glyphicon-chevron-right"></span></a></p>
-        </div>
-        <hr>
-        <div class="container-fluid">
-            <h1>Working with Us</h1>
-            <p>MekDoornels is an equal-opportunities employer. We have a variety of vacancies for many specialties as part of our efforts to expand further and experiment further. If you or a friend need a job, do not refrain from contacting us! As part of our global outreach programme, employees and their families have many special privileges and perks that come with being a part of the MekDoornels family. Some of them include a 30% discount at all MekDoornels restaurants!</p>
-            <p><a class="btn btn-primary" href="contact.php" role="button">Contact us for opportunities <span class="glyphicon glyphicon-chevron-right"></span></a></p>
-            <h2>Culinary Experimenter</h2>
-            <p>At the forefront of this field, we will need a lot of people to help us push the boundaries of food. You can sign us as either one of the following:</p>
-            <ul>
-                <li><span class="bold">Follower</span> to give us ideas that we can consider exploring. Mr X, a customer for 30 years, just suggested asparagus ice cream in the recent few months, and it is now a success on the market!</p>
-                <li><span class="bold">Food connoisseur</span> to help suggest new ways to refine our current food.</li>
-                <li><span class="bold">Food researcher</span> to help spot trends and start trends for new forms of food!</li>
-                <li><span class="bold">Food tester</span> to help judge our food and appraise it for the market! You can either help certify the safety of the food, or appraise its taste!</li>
-            </ul>
-        </div>
-        <hr>
-        <div class="container-fluid" id="link-awards">
-            <h1>Our Awards</h1>
-            <p>Since 2009, our amazing food has won over 40 prizes! Here are some of them which we are most proud of.</p>
-            <table id="awards" class="table">
-            <tr>
-                <th>Year</th>
-                <th>Organization</th>
-                <th>Award</th>
-                <th>Placing</th>
-                <th>Remarks</th>
-            </tr>
-            <tr>
-                <td>2014</td>
-                <td><abbr title="FATS">Food Association for Trade and Safety</abbr></td>
-                <td>Cleanest Restaurants Award</td>
-                <td>1st</td>
-                <td>For the 20th year running</td>
-            </tr>
-            <tr>
-                <td>2013</td>
-                <td>Consumers&lsquo; Union</td>
-                <td>Lowest Fat Prize</td>
-                <td>1st</td>
-                <td>For the 3rd year running</td>
-            </tr>
-            <tr>
-                <td>2014</td>
-                <td>Consumer&lsquo; Union</td>
-                <td>Designer Ice Cream Meet</td>
-                <td>Gold</td>
-                <td></td>
-            </tr>
-            <tr>
-                <td>2012</td>
-                <td>International Foodlympic Federation</td>
-                <td>Titanium Chefs (Team) Event</td>
-                <td>1st</td>
-                <td>For the 5th Foodlympics running</td>
-            </tr>
-            <tr>
-                <td>2014</td>
-                <td><abbr title="FIFA">F&eacute;d&eacute;ration Internationale de Food Association</abbr></td>
-                <td>Quad-yearly Grand Tournament</td>
-                <td>1st</td>
-                <td></td>
-            </tr>
-            </table>
-        </div>
-    </div>
-    !ipp[_cep14_insert components/_footer.html]
+	!ipp[_cep14_insert components/_navbar.html]
+	<div class = "page-header">
+		<h1>About <span class="text-small">so you can get to know more about us and have more confidence in eating our food</span></h1>
+	</div>
+	<div class = "container">
+		<div class="container-fluid">
+			<h1>So, who are we?</h1>
+			<p>We are a group of pioneers who want to get rid of the notion of oily fast food.</p>
+			<p>We believe in low-fat food that can be both enjoyable and tasty. With our new <span class="span-topSecret">[REDACTED]</span> technology, enjoy burgers and fries with unbelievably low fat values!<p>
+			<p><a class="btn btn-primary" href="menu.php" role="button">Low Fat Food Menu <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+		</div>
+		<hr>
+		<div class="container-fluid">
+			<h1>Our Story</h1>
+			<p>MekDoornels started in the year 1939, founded by the LSN Foundation as an experimental restaurant chain. Our first test subjects, er, customers were willing and daring volunteers who signed up for our tasty programme.</p>
+			<p>The restaurant chain was intended to produce food for customers as fast as possible.</p>
+			<p>In the first 6 years of our operation, just as the world struggled to survive through one of the most deadly wars ever, we struggled to stay competitive in the ever-changing restaurant industry.</p>
+			<p>However, ever since Pear Inc. sponsored our growth in the year 1965, our business has continued to skyrocket, until you see the success that is us today.</p>
+			<p>By the year 1984, we have expanded to 3 continents <!--Oceania, Eurasia, Eastasia (1984 reference)--> and our yearly revenue had increased to USD 1 000 000 000 000!</p>
+			<p>Till today, our business still flourishes everywhere we are. We look forward to building a brighter future with you.</p>
+		</div>
+		<hr>
+		<div class="container-fluid">
+			<h1>Our Vision</h1>
+			<p>As we started up as an experimental restaurant chain, we still partake in culinary experiments to give our customers the very best.</p>
+			<p>We intend to pioneer the food industry to bring food to new levels of <span style="font-size: 1pt">questionable </span>enjoyability and <span style="font-size:1pt">zero </span>nutrition.</p>
+			<p>Our breakthrough invention, the <span class="span-topSecret">[REDACTED]</span> technology allows us to reduce fat values in foot to near zero levels, elevating our foodstuffs to a even greater level in health.</p>
+			<p><a class="btn btn-primary" href="menu.php" role="button">Try a Sample <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+			<p>We are also looking for people to take part in our culinary experiments! All our released-for-testing foodstuffs have been extensively tested and proven safe by over 100 organizations, including the <abbr title="FATS">Food Association for Trade and Security</abbr>. Join us to contribute your valuable advice and opinion to help make our food the best for everyone!</p>
+			<p><a class="btn btn-primary" href="contact.php" role="button">Sign up as Culinary Test Subject <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+		</div>
+		<hr>
+		<div class="container-fluid">
+			<h1>Working with Us</h1>
+			<p>MekDoornels is an equal-opportunities employer. We have a variety of vacancies for many specialties as part of our efforts to expand further and experiment further. If you or a friend need a job, do not refrain from contacting us! As part of our global outreach programme, employees and their families have many special privileges and perks that come with being a part of the MekDoornels family. Some of them include a 30% discount at all MekDoornels restaurants!</p>
+			<p><a class="btn btn-primary" href="contact.php" role="button">Contact us for opportunities <span class="glyphicon glyphicon-chevron-right"></span></a></p>
+			<h2>Culinary Experimenter</h2>
+			<p>At the forefront of this field, we will need a lot of people to help us push the boundaries of food. You can sign us as either one of the following:</p>
+			<ul>
+				<li><span class="bold">Follower</span> to give us ideas that we can consider exploring. Mr X, a customer for 30 years, just suggested asparagus ice cream in the recent few months, and it is now a success on the market!</p>
+				<li><span class="bold">Food connoisseur</span> to help suggest new ways to refine our current food.</li>
+				<li><span class="bold">Food researcher</span> to help spot trends and start trends for new forms of food!</li>
+				<li><span class="bold">Food tester</span> to help judge our food and appraise it for the market! You can either help certify the safety of the food, or appraise its taste!</li>
+			</ul>
+		</div>
+		<hr>
+		<div class="container-fluid" id="link-awards">
+			<h1>Our Awards</h1>
+			<p>Since 2009, our amazing food has won over 40 prizes! Here are some of them which we are most proud of.</p>
+			<table id="awards" class="table">
+				<tr>
+					<th>Year</th>
+					<th>Organization</th>
+					<th>Award</th>
+					<th>Placing</th>
+					<th>Remarks</th>
+				</tr>
+				<tr>
+					<td>2014</td>
+					<td><abbr title="FATS">Food Association for Trade and Safety</abbr></td>
+					<td>Cleanest Restaurants Award</td>
+					<td>1st</td>
+					<td>For the 20th year running</td>
+				</tr>
+				<tr>
+					<td>2013</td>
+					<td>Consumers&lsquo; Union</td>
+					<td>Lowest Fat Prize</td>
+					<td>1st</td>
+					<td>For the 3rd year running</td>
+				</tr>
+				<tr>
+					<td>2014</td>
+					<td>Consumer&lsquo; Union</td>
+					<td>Designer Ice Cream Meet</td>
+					<td>Gold</td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>2012</td>
+					<td>International Foodlympic Federation</td>
+					<td>Titanium Chefs (Team) Event</td>
+					<td>1st</td>
+					<td>For the 5th Foodlympics running</td>
+				</tr>
+				<tr>
+					<td>2014</td>
+					<td><abbr title="FIFA">Fédération Internationale de Food Association</abbr></td>
+					<td>Quad-yearly Grand Tournament</td>
+					<td>1st</td>
+					<td></td>
+				</tr>
+		    </table>
+		</div>
+	</div>
+	!ipp[_cep14_insert components/_footer.html]
 </body>
 </html>
