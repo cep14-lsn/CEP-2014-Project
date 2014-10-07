c33c982a81ce846ccafe123ceb8d68ea32248991
leafileaf <leafileaftfm@gmail.com>
2014-10-07 17:42:29 +0800

    Update order_delivery.php

--- order_delivery.php
+++ order_delivery.php
@@ -255,7 +255,7 @@
 								<td>
 									{{ item.food.name + ( item.mode ? item.mode == "alc" ? " / À la carte" : ( " / Set Meal / " + item.side.name + ( item.side.name && item.drink.name ? " + " : "" ) + item.drink.name ) : "" ) }}
 									<abbr title = "All funds raised in this manner go to the Take-a-Wish Foundation." data-ng-hide = "item.side.name || item.drink.name">Paying extra for nothing</abbr>
-									<p data-ng-show = "item.instr.trim()">Special Order: <abbr title = "We reserve the right to choose which requests to process.">{{ item.instr.trim() }}</abbr></p>
+									<p class = "in-the-line" data-ng-show = "item.instr.trim()"><br />Special Order: <abbr title = "We reserve the right to choose which requests to process.">{{ item.instr.trim() }}</abbr></p>
 								</td>
 								<td>{{ itemcost(item) | currency }}<button type = "button" data-ng-click = "removecart(item)" class = "close"><span data-aria-hidden = "true" class="glyphicon glyphicon-remove"></span><span class = "sr-only">Close</span></button></td>
 							</tr>
